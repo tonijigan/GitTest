d69612a4a67135aaf221aab4190a6426760ca9fb
tonijigan <100856397+tonijigan@users.noreply.github.com>
2022-11-20 19:39:39 +0300

    init

--- Assets/Scripts/AlarmSystem.cs
+++ Assets/Scripts/AlarmSystem.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Alarm), typeof(MotionSensor))]
 public class AlarmSystem : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     [SerializeField] private AudioSource _audioSource;
 
+    private const string IsAlarm = "Alarm";
     private Alarm _alarm;
+    private MotionSensor _motionSensor;
 
     private void Start()
     {
         _alarm = GetComponent<Alarm>();
+        _motionSensor = GetComponent<MotionSensor>();
         _audioSource.volume = _audioSource.minDistance;
     }
 
@@ -21,22 +25,22 @@ public class AlarmSystem : MonoBehaviour
 
         if (_audioSource.volume == _audioSource.minDistance)
         {
-            _animator.SetBool("Alarm", false);
             _audioSource.Stop();
+            _animator.SetBool(IsAlarm, false);
             StopCoroutine(_alarm.ChangeVolume(_audioSource));
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        _animator.SetBool("Alarm", true);
-        _audioSource.Play();
         _alarm.PlayAlarm();
+        _audioSource.Play();
+        _animator.SetBool(IsAlarm, true);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        _audioSource.volume = 1;
         _alarm.StopAlarm();
+        _audioSource.volume = 1;
     }
 }
